6a49d59dbf7bff7e5871a4afd0e2626182f0bb4b
中村真澄 <monoism@gmail.com>
2019-02-24 17:10:24 +0900

    add dev

---
+++ webpack.config.prod.js
@@ -0,0 +1,38 @@
+module.exports = {
+	mode: 'production',
+	entry: {
+		'./docs/assets/javascript/bundle': './src/assets/javascript/main.ts'
+	},
+	output: {
+		path: __dirname,
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			{
+				enforce: 'pre',
+				test: /\.ts?$/,
+				use: [
+					{
+						loader: 'tslint-loader',
+						options: {
+							typeCheck: true,
+							fix: true
+						}
+					}
+				]
+			}, {
+				// 拡張子 .ts の場合
+				test: /\.ts$/,
+				// TypeScript をコンパイルする
+				use: 'ts-loader'
+			}
+		]
+	},
+	// import 文で .ts ファイルを解決するため
+	resolve: {
+		extensions: [
+			'.ts'
+		]
+	}
+};
