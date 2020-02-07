bf05fb657364f545ae67e51a770e5f1c1ebdc850
中村真澄 <monoism@gmail.com>
2020-02-08 02:41:18 +0900

    インデントの修正

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,40 +1,40 @@
 module.exports = {
-	mode: 'development',
-	devtool: 'inline-source-map',
-	// メインとなるJavaScriptファイル（エントリーポイント）
-	entry: {
-		'./docs/assets/js/bundle': './src/assets/js/main.ts'
-	},
-	output: {
-		path: __dirname,
-		filename: '[name].js'
-	},
-	module: {
-		rules: [
-			{
-				enforce: 'pre',
-				test: /\.ts?$/,
-				use: [
-					{
-						loader: 'tslint-loader',
-						options: {
-							typeCheck: true,
-							fix: true
-						}
-					}
-				]
-			}, {
-				// 拡張子 .ts の場合
-				test: /\.ts$/,
-				// TypeScript をコンパイルする
-				use: 'ts-loader'
-			}
-		]
-	},
-	// import 文で .ts ファイルを解決するため
-	resolve: {
-		extensions: [
-			'.ts'
-		]
-	}
+    mode: 'development',
+    devtool: 'inline-source-map',
+    // メインとなるJavaScriptファイル（エントリーポイント）
+    entry: {
+        './docs/assets/js/bundle': './src/assets/js/main.ts'
+    },
+    output: {
+        path: __dirname,
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.ts?$/,
+                use: [
+                    {
+                        loader: 'tslint-loader',
+                        options: {
+                            typeCheck: true,
+                            fix: true
+                        }
+                    }
+                ]
+            }, {
+                // 拡張子 .ts の場合
+                test: /\.ts$/,
+                // TypeScript をコンパイルする
+                use: 'ts-loader'
+            }
+        ]
+    },
+    // import 文で .ts ファイルを解決するため
+    resolve: {
+        extensions: [
+            '.ts'
+        ]
+    }
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,38 +1,38 @@
 module.exports = {
-	mode: 'production',
-	entry: {
-		'./docs/assets/js/bundle': './src/assets/js/main.ts'
-	},
-	output: {
-		path: __dirname,
-		filename: '[name].js'
-	},
-	module: {
-		rules: [
-			{
-				enforce: 'pre',
-				test: /\.ts?$/,
-				use: [
-					{
-						loader: 'tslint-loader',
-						options: {
-							typeCheck: true,
-							fix: true
-						}
-					}
-				]
-			}, {
-				// 拡張子 .ts の場合
-				test: /\.ts$/,
-				// TypeScript をコンパイルする
-				use: 'ts-loader'
-			}
-		]
-	},
-	// import 文で .ts ファイルを解決するため
-	resolve: {
-		extensions: [
-			'.ts'
-		]
-	}
+    mode: 'production',
+    entry: {
+        './docs/assets/js/bundle': './src/assets/js/main.ts'
+    },
+    output: {
+        path: __dirname,
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.ts?$/,
+                use: [
+                    {
+                        loader: 'tslint-loader',
+                        options: {
+                            typeCheck: true,
+                            fix: true
+                        }
+                    }
+                ]
+            }, {
+                // 拡張子 .ts の場合
+                test: /\.ts$/,
+                // TypeScript をコンパイルする
+                use: 'ts-loader'
+            }
+        ]
+    },
+    // import 文で .ts ファイルを解決するため
+    resolve: {
+        extensions: [
+            '.ts'
+        ]
+    }
 };
