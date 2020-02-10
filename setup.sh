ad13b16dfe508266b4f42a1b8d7f67d11b1d80d9
中村真澄 <monoism@gmail.com>
2020-02-11 04:57:45 +0900

    setup調整

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 curl -L -O https://github.com/monowebjp/frontend-static-boilerplate/archive/master.zip
 unzip master.zip
 rm master.zip
+rm frontend-static-boilerplate-master/README.md
+rm frontend-static-boilerplate-master/setup.sh
 mv frontend-static-boilerplate-master/* .
+mv frontend-static-boilerplate-master/.editorconfig ./.editorconfig
+mv frontend-static-boilerplate-master/.eslintrc.json ./.eslintrc.json
+mv frontend-static-boilerplate-master/.gitignore ./.gitignore
+mv frontend-static-boilerplate-master/.stylelintrc ./.stylelintrc
+rm -r frontend-static-boilerplate-master
