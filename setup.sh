ce6ea8915c6889b55e68bb47589bd2c61c8a18cf
中村真澄 <monoism@gmail.com>
2020-02-11 05:23:05 +0900

    hostsの書き換えも追加したい

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
+#!/bin/sh
 curl -L -O https://github.com/monowebjp/frontend-static-boilerplate/archive/master.zip
+
 unzip master.zip
+
 rm master.zip
 rm frontend-static-boilerplate-master/README.md
 rm frontend-static-boilerplate-master/setup.sh
+
 mv frontend-static-boilerplate-master/* .
 mv frontend-static-boilerplate-master/.editorconfig ./.editorconfig
 mv frontend-static-boilerplate-master/.eslintrc.json ./.eslintrc.json
@@ -10,6 +14,8 @@ mv frontend-static-boilerplate-master/.gitignore ./.gitignore
 mv frontend-static-boilerplate-master/.stylelintrc ./.stylelintrc
 rm -r frontend-static-boilerplate-master
 
-echo $#
-echo $1
 sed -i -e "s/LOCAL_HOST_NAME/$1/g" ./bs-config.js
+sed -i -e "s/LOCAL_HOST_NAME/$1/g" ./docker-compose.yml
+sed -i -e "s/LOCAL_HOST_NAME/$1/g" ./docker/nginx.conf
+
+rm setup.sh
