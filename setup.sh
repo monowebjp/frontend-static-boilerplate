70d0fa9f7428e98137eec9936a79fd921f93e6d3
中村真澄 <monoism@gmail.com>
2020-02-11 16:31:23 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -39,20 +39,20 @@ sed -i -e "s/LOCAL_HOST_NAME/$HOST_NAME/g" ./bs-config.js
 sed -i -e "s/LOCAL_HOST_NAME/$HOST_NAME/g" ./docker-compose.yml
 sed -i -e "s/LOCAL_HOST_NAME/$HOST_NAME/g" ./docker/nginx.conf
 
-git config --get remote.origin.url
-sed -i -e "s/https:\/\/github.com\/monowebjp\/frontend-static-boilerplate.git/$?/g" ./package.json
+REPOSITORY_URL=$(git config --get remote.origin.url)
+sed -i -e "s/https:\/\/github.com\/monowebjp\/frontend-static-boilerplate.git/$REPOSITORY_URL/g" ./package.json
 
 sed -i -e "s/frontend-static-boilerplate/$PROJECT_NAME/g" ./package.json
 sed -i -e "s/PROJECT_NAME/$PROJECT_NAME/g" ./README.md
 
 rm *-e
 
+rm setup.sh
+
+find . -exec chmod 777 {} +
+
 yarn install
 docker-compose up -d
 npm start
 
-find . -exec chmod 777 {} +
-
 echo '環境を構築しました'
-
-rm setup.sh
